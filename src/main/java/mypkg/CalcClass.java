e103152a8a8823c5522d9ce9f5f5f63b49ce0f77
Aishik Biswas <aishik.biswas@DDN.UPES.AC.IN>
2024-07-10 14:57:30 +0530

    added mult2() function

--- src/main/java/mypkg/CalcClass.java
+++ src/main/java/mypkg/CalcClass.java
@@ -18,4 +18,18 @@ public class CalcClass {
 	{
 		return a/b;
 	}
+	public int mult2(int a,int b)
+	{
+		int mul=1;
+		for(int i=1;i<=a;i++)
+		{
+			mult=mult*i;
+		}
+		int mul2=1;
+		for(int i=1;i<=b;i++)
+		{
+			mult=mult*i;
+		}
+		return a+b;
+	}
 }
